e5bb64e17f83e8a22cf83b4ccfbb4073d08699f9
seorin-yu <seorin.yu@navercorp.com>
2025-11-09 18:54:53 +0900

    init: cors 추가

---
+++ src/main/kotlin/hidc/seorin/hidcserver/config/CorsConfig.kt
@@ -0,0 +1,18 @@
+package hidc.seorin.hidcserver.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600)
+    }
+}
+
