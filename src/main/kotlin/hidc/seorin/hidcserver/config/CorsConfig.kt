7535342dbffd394e4fbe4614aa810aefd7e4ee1a
seorin-yu <seorin.yu@navercorp.com>
2025-11-23 17:33:21 +0900

    add cors

--- src/main/kotlin/hidc/seorin/hidcserver/config/CorsConfig.kt
+++ src/main/kotlin/hidc/seorin/hidcserver/config/CorsConfig.kt
@@ -8,19 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class CorsConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOrigins(
-                "https://api.hidcgs2025.com",
-                "http://api.hidcgs2025.com",
-                "https://hidcgs2025.com",
-                "http://hidcgs2025.com",
-                "https://hidc-client.pages.dev",
-                "http://localhost:3000",
-                "http://localhost:5173"
-            )
+            .allowedOrigins("*")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
             .allowedHeaders("*")
-            .allowCredentials(true)
-            .exposedHeaders("*")
             .maxAge(3600)
     }
 }
