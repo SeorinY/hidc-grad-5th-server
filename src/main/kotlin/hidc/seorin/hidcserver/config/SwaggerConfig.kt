b39ac682fff45ce577f6b89cb0c5b0183c3e243c
seorin-yu <seorin.yu@navercorp.com>
2025-11-23 17:54:24 +0900

    add cors

--- src/main/kotlin/hidc/seorin/hidcserver/config/SwaggerConfig.kt
+++ src/main/kotlin/hidc/seorin/hidcserver/config/SwaggerConfig.kt
@@ -2,6 +2,7 @@ package hidc.seorin.hidcserver.config
 
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.servers.Server
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -16,6 +17,11 @@ class SwaggerConfig {
                     .title("HIDC Grad 5th API")
                     .description("HIDC Grad 5th Server API Documentation")
                     .version("1.0.0")
+            )
+            .servers(
+                listOf(
+                    Server().url("https://api.hidcgs2025.com").description("Production Server"),
+                )
             )
     }
 }
